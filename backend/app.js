006bec3308bb4f2d39ee78dcc29473059f34268b
Bryan 'Max' Dobberstein <max.dobberstein@here.com>
2021-12-08 19:59:01 -0600

    add env dev pack fix syntax error (cors > cors())

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,26 @@
+require('dotenv').config();
 const bp = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const connectionString = require('./database/db');
-
 const app = express();
 
 app.use(bp.json());
-app.use(cors);
+app.use(cors());
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.json({ connection: 'Works' });
+});
 
 mongoose
-  .connect(connectionString)
-  .then(result => {
+  .connect(process.env.DB_URL)
+  .then(() => {
     console.log('Mongo is only pawn in game of life');
     app.listen(PORT, () => {
-      if (PORT === 8000) {
-        console.log(
-          `api started on http://localhost:${PORT}`
-        );
-      } else {
-        console.log(`api started on ${PORT}`);
-      }
+      console.log(`api started on ${PORT}`);
     });
   })
   .catch(err => {
