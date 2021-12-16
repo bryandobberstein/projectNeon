d940d1d71753180fca85abff93d6e743310c7745
Bryan 'Max' Dobberstein <max.dobberstein@here.com>
2021-12-15 21:48:55 -0600

    initial commit of cleaned up base react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>Under Construction</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Fugiat, placeat. Omnis assumenda error nobis
+        atque ratione dolor vitae quas, mollitia facere
+        pariatur autem, quidem officia illum cumque ad
+        blanditiis at.
+      </p>
+    </div>
+  );
+};
+
+export default App;
