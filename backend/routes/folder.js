369db73569641857d418b9b8157c07c188a0a51a
Bryan 'Max' Dobberstein <max.dobberstein@here.com>
2021-12-27 16:33:33 -0600

    fix syntax error in response

--- backend/routes/folder.js
+++ backend/routes/folder.js
@@ -6,7 +6,7 @@ const tokenVerify = require('../middleware/tokenVerify');
 const router = express.Router();
 
 router.post('/getFolders', tokenVerify, (req, res) => {
-  res.status(200).send(true).json({ msg: 'success' });
+  res.status(200).json({ msg: 'success' });
 });
 
 module.exports = router;
